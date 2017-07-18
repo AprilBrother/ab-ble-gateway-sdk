cddd27c17d8f73f4f3326e8b1401ab249a223d82
volca <volca@tom.com>
2017-07-18 12:19:05 +0800

    mqtt client

--- tools/mqtt-client/nodejs/mqtt-client.js
+++ tools/mqtt-client/nodejs/mqtt-client.js
@@ -5,7 +5,12 @@ client.on('connect', function () {
   client.subscribe('beacons');
 });
  
-client.on('message', function (topic, message) {
-     // message is Buffer 
-      console.log(message.toString());
+client.on('message', function (topic, data) {
+    data = data.toString('hex');
+    const head = decodeURIComponent(data.match(/^7b\S+0d0a0d0a/)[0].replace(/(\w{2})/g, '%$1'));
+    data = data.replace(head, '');
+    data = data.replace(/(0d0a)/gm, '$1\n');
+    const bles = data.match(/fe\w+0d0a/gm);
+    console.log(head)   // meta data
+    console.log(bles)   // BLE advertisement data
 });
