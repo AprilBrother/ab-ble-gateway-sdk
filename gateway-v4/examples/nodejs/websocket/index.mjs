cd76d76e9faac336753be128a538662c9151e0ac
yanc <volcas@gmail.com>
2024-04-19 11:16:52 +0800

    update websocket server example

--- gateway-v4/examples/nodejs/websocket/index.mjs
+++ gateway-v4/examples/nodejs/websocket/index.mjs
@@ -1,14 +1,22 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from 'ws'
+import msgpack5 from 'msgpack5'
 
 const port = 8080
 const wss = new WebSocketServer({ port: port });
+const msgpack = msgpack5()
 
 console.log('Websocket server started at port:', port);
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+    ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+    ws.on('message', function message(body) {
+        try {
+            let data = JSON.parse(body)
+            console.log("JSON:", data)
+        } catch(e) {
+            let data = msgpack.decode(body)
+            console.log("MsgPack:", data)
+        }
+    });
 
 });
